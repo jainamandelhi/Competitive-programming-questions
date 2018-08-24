06a898a878e4c6648e27e6c04c7c45d5b27eeb3d
jainamandelhi <39856232+jainamandelhi@users.noreply.github.com>
2018-08-24 22:58:18 +0530

    Codeforces

---
+++ Insomnia cure.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[5];
+    for(int i=0;i<5;i++)
+        cin>>arr[i];
+    vector<int>arr1(arr[4]+1,0);
+    for(int i=0;i<4;i++)
+    {
+        for(int j=arr[i];j<=arr[4];j=j+arr[i])
+            arr1[j]++;
+    }
+    int count=0;
+    for(int i=0;i<=arr[4];i++)
+    {
+        if(arr1[i]!=0)
+            count++;
+    }
+    cout<<count;
+return 0;
+}
