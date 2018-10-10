ae488cf58aab598347137fa411292cd8882d2a32
Aman Kumar Jain <39856232+jainamandelhi@users.noreply.github.com>
2018-10-10 12:39:13 +0530

    Codeforces

---
+++ Little Elephant and Function.cpp
@@ -0,0 +1,15 @@
+/*
+Aur doobna hai ya koodna zaruri tha?
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<n<<" ";
+    n--;
+    for(int i=1;i<=n;i++)
+        cout<<i<<" ";
+    return 0;
+}
