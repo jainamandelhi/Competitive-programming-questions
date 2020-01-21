48c3361a2305d94b5d4d081ba64d67b6d9750c43
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-01-22 00:14:10 +0530

    Create Next Round.cpp

---
+++ VK Cup 2012 Qualification Round 1/Next Round.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define mod 1000000007
+using namespace std;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    ll n,k;
+    cin>>n>>k;
+    n;
+    k--;
+    vector<ll>arr(n);
+    for(ll i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr.rbegin(),arr.rend());
+    ll a=arr[k];
+    ll ans=0;
+    for(ll i=0;i<n;i++)
+    {
+        if(arr[i]<a || arr[i]==0)
+            break;
+        ans++;
+    }
+    cout<<ans;
+}
