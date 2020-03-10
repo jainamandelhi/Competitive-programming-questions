c3f01a3e18d275ae0d959392d979b879015ca8ad
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-03-11 01:11:17 +0530

    Create A. Two Regular Polygons.cpp

---
+++ Educational Codeforces Round 83 (Rated for Div. 2)/A. Two Regular Polygons.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define mod 1000000007
+using namespace std;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m;
+        cin>>n>>m;
+        if(n%m)
+            cout<<"NO";
+        else
+            cout<<"YES";
+        cout<<endl;
+    }
+}
