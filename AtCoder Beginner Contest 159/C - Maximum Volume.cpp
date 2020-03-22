38153bd3b168f1c8ac7cc88002e915976b1b94f7
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-03-23 01:28:45 +0530

    Create C - Maximum Volume.cpp

---
+++ AtCoder Beginner Contest 159/C - Maximum Volume.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define mod 1000000007
+using namespace std;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    double n;
+    cin>>n;
+    double a = n/3;
+    a = a*a*a;
+    cout<<fixed<<setprecision(9)<<a;
+}
