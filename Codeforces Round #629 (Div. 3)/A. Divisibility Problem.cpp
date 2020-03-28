770f3ffb5a3a69ec91c6250c606a81ea8c0626a7
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-03-28 17:03:37 +0530

    Create A. Divisibility Problem.cpp

---
+++ Codeforces Round #629 (Div. 3)/A. Divisibility Problem.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define mod 1000000007
+using namespace std;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m;
+        cin>>n>>m;
+        if(n%m == 0)
+            cout<<0;
+        else
+            cout<<m-n%m;
+        cout<<endl;
+    }
+}
