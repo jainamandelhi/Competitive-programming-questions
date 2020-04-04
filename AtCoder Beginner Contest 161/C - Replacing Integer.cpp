4ea5b2f4ebaedd216037e60539758f4ea322574c
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-04-04 18:45:16 +0530

    Create C - Replacing Integer.cpp

---
+++ AtCoder Beginner Contest 161/C - Replacing Integer.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define mod 998244353
+using namespace std;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    ll n,m;
+    cin>>n>>m;
+    ll temp = n/m;
+    cout<<min(abs(n-temp*m),abs(n-(temp+1)*m));
+}
