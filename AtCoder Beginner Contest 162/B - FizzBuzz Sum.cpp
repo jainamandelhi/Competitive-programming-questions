6fff91c7e1f7147df02dc73ae3e6cd4062bac85a
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-04-13 01:14:40 +0530

    Create B - FizzBuzz Sum.cpp

---
+++ AtCoder Beginner Contest 162/B - FizzBuzz Sum.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define mod 1000000007
+using namespace std;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    ll n;
+    cin>>n;
+    ll ans = 0;
+    for(ll i=1;i<=n;i++)
+    {
+        if(i%3 && i%5)
+            ans += i;
+    }
+    cout<<ans;
+}
