d5cc575bbefbede823c1d58c291542da49ae54cb
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-04-19 03:10:21 +0530

    Create Leaked Answers (Challenge).cpp

---
+++ April Challenge 2020 Division 1/Leaked Answers (Challenge).cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define mod 998244353
+using namespace std;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+
+}
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m,k;
+        cin>>n>>m>>k;
+        ll arr[n][k];
+        for(ll i = 0; i < n; i++)
+        {
+            for(ll j = 0; j < k; j++)
+                cin>>arr[i][j];
+        }
+        ll itr = 0;
+        vector<ll>ans;
+        for(ll i = 0; i < n; i++)
+        {
+            ans.pb(arr[i][itr%k]);
+            itr++;
+        }
+        for(ll i = 0; i < ans.size(); i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+}
