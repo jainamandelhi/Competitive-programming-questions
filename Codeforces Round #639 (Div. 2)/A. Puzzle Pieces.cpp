c4b5cc7748bed640c42c4c6b9ccd37520034d6eb
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-05-07 17:00:30 +0530

    Create A. Puzzle Pieces.cpp

---
+++ Codeforces Round #639 (Div. 2)/A. Puzzle Pieces.cpp
@@ -0,0 +1,34 @@
+/* Isn't passion overrated?*/
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define mod 1000000007
+using namespace std;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m;
+        cin>>n>>m;
+        if(n == 1 || m == 1 )
+            cout<<"YES";
+        else if(n == 2 && m == 2)
+            cout<<"YES";
+        else
+            cout<<"NO";
+        cout<<endl;
+    }
+    return 0;
+}
