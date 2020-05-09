7163a390740392b0a695f2518281f9c401c51650
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-05-10 01:01:52 +0530

    Create C. K-th Not Divisible by n.cpp

---
+++ Codeforces Round #640 (Div. 4)/C. K-th Not Divisible by n.cpp
@@ -0,0 +1,31 @@
+/* Isn't passion overrated?*/
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define mod 1000000007
+using namespace std;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,k;
+        cin>>n>>k;
+        ll ans = (k*n)/(n-1);
+        if((k*n) % (n-1) == 0)
+           ans--;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
