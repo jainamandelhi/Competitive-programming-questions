a0bc185c9078b5ecaa237e9c23a19a38805da25a
Aman Kumar Jain <jainamandelhi@gmail.com>
2020-05-10 19:23:16 +0530

    Create B - Easy Linear Programming.cpp

---
+++ AtCoder Beginner Contest 167/B - Easy Linear Programming.cpp
@@ -0,0 +1,35 @@
+/* Isn't passion overrated?*/
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define mod 1000000007
+using namespace std;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+int main()
+{
+    ios_base:: sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    ll a,b,c,k;
+    cin>>a>>b>>c>>k;
+    ll sum = 0;
+    ll t = min(k,a);
+    sum += t;
+    k -= t;
+    a -= t;
+    t = min(k,b);
+    k -= t;
+    b -= t;
+    t = min(k,c);
+    sum -= t;
+    k -= t;
+    c -= t;
+    cout<<sum;
+    return 0;
+}
